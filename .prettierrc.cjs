6f4ed330cdfba1e54e9549c6d9a291ebdff1a400
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-05-03 17:04:53 +0600

    Migrate to Vite

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 120,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  insertPragma: false,
+  endOfLine: 'lf',
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  trailingComma: 'none',
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrderSeparation: true,
+  importOrder: ['^react', '^@mui/(.*)$', '^@/(.*)$', '^[./]']
+};
--- src/typings/index.ts
+++
@@ -1,7 +0,0 @@
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-
-export interface ContactInfo {
-  tag: "telegram" | "github" | "email";
-  href: string;
-  icon: IconProp;
-}
