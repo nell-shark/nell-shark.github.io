5158d33cbf89bcadd11eb609992ca22e5be06f71
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-05-05 05:02:40 +0600

    Create Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+  return (
+    <footer className='mt-8 pb-5'>
+      <hr className='border-gray-400 mb-4 w-full' />
+      <div className='flex justify-between'>
+        <span className='text-sm dark:text-gray-400'>@2025 | Krivolapov Vladislav</span>
+        <span className='text-sm dark:text-gray-400'>Made with React + Vite + Tailwind = ❤️</span>
+      </div>
+    </footer>
+  );
+}
