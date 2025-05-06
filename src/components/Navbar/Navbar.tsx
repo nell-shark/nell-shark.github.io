9e3949e68d6d3bfc7ca067ad4eaddf322483b12e
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-05-06 06:39:40 +0600

    Add Home item to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,8 @@ import Logo from '@/assets/logo.svg';
 import { NavbarItem } from '@/components/Navbar/NavbarItem';
 
 const NAVBAR_ITEMS = [
-  { label: 'About me', href: '#about-me' },
+  { label: 'Home', href: '#home' },
+  { label: 'About', href: '#about-me' },
   { label: 'Skills', href: '#skills' },
   { label: 'Projects', href: '#projects' },
   { label: 'Contact', href: '#contact' }
@@ -29,14 +30,14 @@ export function Navbar() {
 
   return (
     <header
-      className={`fixed inset-x-0 top-4 z-50 h-[60px] rounded-3xl border px-5 shadow-sm backdrop-blur-[10px] mx-auto w-full max-w-screen-lg transition-colors duration-300 border-transparent ${background}`}
+      className={`fixed inset-x-0 top-4 z-50 mx-auto h-[60px] w-full max-w-screen-lg rounded-3xl border border-transparent px-5 shadow-sm backdrop-blur-[10px] transition-colors duration-300 ${background}`}
     >
-      <div className='w-full mx-auto flex h-[60px] items-center justify-between'>
+      <div className='mx-auto flex h-[60px] w-full items-center justify-between'>
         <img src={Logo} alt='Logo' width='40' height='40' />
         <nav className='flex items-center space-x-3'>
-          <ul className='space-x-2 flex'>
+          <ul className='flex space-x-2'>
             {NAVBAR_ITEMS.map(({ href, label }) => (
-              <NavbarItem key={href} label={label} href={`#${label.replace(' ', '-').toLowerCase()}`} />
+              <NavbarItem key={href} label={label} href={`#${label.toLowerCase()}`} />
             ))}
           </ul>
         </nav>
--- src/components/Navbar/NavbarItem/NavbarItem.tsx
+++ src/components/Navbar/NavbarItem/NavbarItem.tsx
@@ -8,8 +8,7 @@ export function NavbarItem({ label, href }: Readonly<NavbarItemProps>) {
     <li>
       <a
         href={href}
-        className='inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-2 text-[rgb(159,159,159)]
-        transition-colors duration-400 hover:text-white'
+        className='inline-flex items-center justify-center rounded-md px-3 py-2 whitespace-nowrap text-[rgb(159,159,159)] transition-colors duration-400 hover:text-white'
       >
         {label}
       </a>
