6d5875fbe6a0c0060e1a2da319881a16b3136a45
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-05-06 06:23:01 +0600

    Add contact information constants for GitHub, Telegram, and email

---
+++ src/data/info.tsx
@@ -0,0 +1,3 @@
+export const GITHUB_PROFILE_URL = 'https://github.com/nell-shark';
+export const TELEGRAM_PROFILE_URL = 'https://t.me/nellshark';
+export const EMAIL_URL = 'mailto:KrivolapovVladislav1998@gmail.com';
