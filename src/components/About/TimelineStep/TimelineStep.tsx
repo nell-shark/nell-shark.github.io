b9255687a2de5098b865a0196b41148970f6762c
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-05-09 12:29:54 +0600

    Refactor TimelineStep to use framer-motion for animations and improve visibility on scroll

--- src/components/About/TimelineStep/TimelineStep.tsx
+++ src/components/About/TimelineStep/TimelineStep.tsx
@@ -1,3 +1,6 @@
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
+
 type TimelineStepProps = {
   year: number;
   title: string;
@@ -5,11 +8,20 @@ type TimelineStepProps = {
 };
 
 export function TimelineStep({ year, title, text }: Readonly<TimelineStepProps>) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
-    <div className='flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:left-[-35px] sm:before:z-[1] sm:before:h-4 sm:before:w-4 sm:before:rounded-full before:dark:bg-violet-600'>
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, y: 50 }}
+      animate={isInView ? { opacity: 1, y: 0 } : {}}
+      transition={{ duration: 0.6, ease: 'easeOut' }}
+      className='flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:left-[-35px] sm:before:z-[1] sm:before:h-4 sm:before:w-4 sm:before:rounded-full before:dark:bg-violet-600'
+    >
       <h3 className='text-xl font-semibold tracking-wide text-white'>{title}</h3>
       <time className='text-xs tracking-wide uppercase dark:text-gray-600'>{year}</time>
       <p className='mt-3 text-gray-300'>{text}</p>
-    </div>
+    </motion.div>
   );
 }
