adfb313bfcdb7db2572ae95535211153bda9b389
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-05-11 09:34:19 +0600

    Refactor Footer component layout for improved styling and alignment

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <footer className='mt-8 pb-5'>
-      <hr className='border-gray-400 mb-4 w-full' />
+    <footer className='mx-auto w-2/3 pb-5'>
+      <hr className='mb-4 w-full border-gray-400' />
       <div className='flex justify-between'>
         <span className='text-sm dark:text-gray-400'>@2025 | Krivolapov Vladislav</span>
         <span className='text-sm dark:text-gray-400'>Made with React + Vite + Tailwind = ❤️</span>
