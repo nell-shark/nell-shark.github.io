9709db7b2fa068629bda134104e8c5399caf0ba2
Krivolapov Vladislav <krivolapovvladislav1998@gmail.com>
2025-05-15 23:23:16 +0600

    Add id in Projects.tsx

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,7 +2,7 @@ import { EmblaCarousel } from '@/components/Projects/EmblaCarousel';
 
 export function Projects() {
   return (
-    <section className='flex min-h-[100vh] items-center justify-center'>
+    <section id='projects' className='flex min-h-[100vh] items-center justify-center'>
       <EmblaCarousel />
     </section>
   );
