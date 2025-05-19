5e269407e31b20a69289be40ebeecbf28db14046
Krivolapov Vladislav <KrivolapovVladislav1998@gmail.com>
2025-05-19 13:10:44 +0600

    Add responsive styles to Footer component for better layout on smaller screens

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,10 @@
 export function Footer() {
   return (
-    <footer className='mx-auto w-2/3 pb-5'>
+    <footer className='mx-auto w-2/3 pb-5 max-md:w-full'>
       <hr className='mb-4 w-full border-gray-400' />
-      <div className='flex justify-between'>
+      <div className='flex justify-between max-md:justify-center'>
         <span className='text-sm dark:text-gray-400'>@2025 | Krivolapov Vladislav</span>
-        <span className='text-sm dark:text-gray-400'>Made with React + Vite + Tailwind = ❤️</span>
+        <span className='text-sm max-md:hidden dark:text-gray-400'>Made with React + Vite + Tailwind = ❤️</span>
       </div>
     </footer>
   );
